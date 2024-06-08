3760b6fa110c572d08b2c7c1cf89f05d5790596d
cororing <corock.dev@gmail.com>
2024-06-08 16:00:49 +0900

    feat: 사양을 변경할 때 송곳니를 드러내는 악마

---
+++ corock/src/main/java/com/cowaine/corock/chapter01/messaging/MessageManager.java
@@ -0,0 +1,30 @@
+package com.cowaine.corock.chapter01.messaging;
+
+import com.cowaine.corock.chapter01.contract.ContractAmount;
+
+import java.math.BigDecimal;
+
+// 계약을 관리하는 클래스
+public class MessageManager {
+    public ContractAmount contractAmount;
+
+    // 세금 포함 금액 계산
+    public int calculateAmountIncludingTax(int amountExcludingTax, BigDecimal salesTaxRate) {
+        BigDecimal multiplier = salesTaxRate.add(new BigDecimal("1.0"));
+        BigDecimal amountIncludingTax = multiplier.multiply(new BigDecimal(amountExcludingTax));
+        return amountIncludingTax.intValue();
+    }
+
+    // 계약 체결
+    public void conclude() {
+        int amountExcludingTax = 0;
+        BigDecimal salesTaxRate = null;
+
+        // 생략
+        int amountIncludingTax = calculateAmountIncludingTax(amountExcludingTax, salesTaxRate);
+        contractAmount = new ContractAmount();
+        contractAmount.amountIncludingTax = amountIncludingTax;
+        contractAmount.salesTaxRate = salesTaxRate;
+        // 생략
+    }
+}
