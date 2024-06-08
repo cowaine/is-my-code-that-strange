d638de871f6145ed5f710892589170e245af0fc1
cororing <corock.dev@gmail.com>
2024-06-08 16:03:11 +0900

    feat: 초기화되지 않은 쓰레기 객체

--- corock/src/main/java/com/cowaine/corock/Book.java
+++ corock/src/main/java/com/cowaine/corock/Book.java
@@ -1,12 +1,20 @@
 package com.cowaine.corock;
 
+import com.cowaine.corock.chapter01.contract.ContractAmount;
 import com.cowaine.corock.chapter01.game.Magic;
 import com.cowaine.corock.chapter01.game.Member;
 import com.cowaine.corock.chapter01.game.RpgService;
 
-public class Main {
+public class Book {
 
     public static void main(String[] args) {
+        // Book.p4();
+
+        ContractAmount amount = new ContractAmount();
+        System.out.println(amount.salesTaxRate.toString());
+    }
+
+    private static void p4() {
         RpgService rpgService = new RpgService();
         Member member = new Member(10, 35);
         Magic magic = new Magic(25);
