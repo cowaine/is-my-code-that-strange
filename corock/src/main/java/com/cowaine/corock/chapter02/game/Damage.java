912593fe6376b5291cb74955bc5b5ab34c160799
cororing <corock.dev@gmail.com>
2024-06-10 23:26:30 +0900

    refactor: 의미를 알기 쉽게 메서드로 만들고, 메서드를 호출하는 형태로 개선

--- corock/src/main/java/com/cowaine/corock/chapter02/game/Damage.java
+++ corock/src/main/java/com/cowaine/corock/chapter02/game/Damage.java
@@ -2,14 +2,30 @@ package com.cowaine.corock.chapter02.game;
 
 public class Damage {
 
-    public int calculate(int playerArmPower, int playerWeaponPower, int enemyBodyDefense, int enemyArmorDefense) {
-        int totalPlayerAttackPower = playerArmPower + playerWeaponPower;
-        int totalEnemyDefense = enemyBodyDefense + enemyArmorDefense;
+    // 플레이어의 공격력 합계 계산
+    private int sumUpPlayerAttackPower(int playerArmPower, int playerWeaponPower) {
+        return playerArmPower + playerWeaponPower;
+    }
 
+    // 적의 방어력 합계 계산
+    private int sumUpEnemyDefense(int enemyBodyDefense, int enemyArmorDefense) {
+        return enemyBodyDefense + enemyArmorDefense;
+    }
+
+    // 데미지 평가
+    private int estimateDamage(int totalPlayerAttackPower, int totalEnemyDefense) {
         int damageAmount = totalPlayerAttackPower - (totalEnemyDefense / 2);
         if (damageAmount < 0) {
             damageAmount = 0;
         }
+
+        return damageAmount;
+    }
+
+    public int calculate(int playerArmPower, int playerWeaponPower, int enemyBodyDefense, int enemyArmorDefense) {
+        int totalPlayerAttackPower = sumUpPlayerAttackPower(playerArmPower, playerWeaponPower);
+        int totalEnemyDefense = sumUpEnemyDefense(enemyBodyDefense, enemyArmorDefense);
+        int damageAmount = estimateDamage(totalPlayerAttackPower, totalEnemyDefense);
 
         return damageAmount;
     }
