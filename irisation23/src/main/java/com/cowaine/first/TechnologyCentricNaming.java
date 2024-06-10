6baee571a17fce7c3ec291f570b3f1c68d6c5f39
bunsung92 <bunsung92@naver.com>
2024-06-10 15:11:50 +0900

    feat: 의미를 알 수 없는 이름(기술 중심 명명, 일련번호 명명) 코드 추가

---
+++ irisation23/src/main/java/com/cowaine/first/TechnologyCentricNaming.java
@@ -0,0 +1,18 @@
+package com.cowaine.first;
+
+public class TechnologyCentricNaming {
+}
+
+class MemoryStateManager {
+    private int intValue01;
+    private boolean flag02;
+    void changeIntValue01(int changeValue) {
+        intValue01 -= changeValue;
+        updateState02Flag();
+    }
+
+    void updateState02Flag() {
+        flag02 = !flag02;
+    }
+}
+
