5135f3f3ab21061ffea45dd3486189b9b02c9f5e
cororing <corock.dev@gmail.com>
2024-06-10 23:02:47 +0900

    feat: 어떤 기능을 하는지 이해하기 힘든 로직

--- corock/src/main/java/com/cowaine/corock/Book.java
+++ corock/src/main/java/com/cowaine/corock/Book.java
@@ -4,6 +4,7 @@ import com.cowaine.corock.chapter01.contract.ContractAmount;
 import com.cowaine.corock.chapter01.game.Magic;
 import com.cowaine.corock.chapter01.game.Member;
 import com.cowaine.corock.chapter01.game.RpgService;
+import com.cowaine.corock.chapter02.game.Mystery;
 import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
@@ -13,11 +14,10 @@ public class Book {
 
     public static void main(String[] args) {
         // Book.p4();
+        // Book.p10();
 
-        int amountIncludingTax = 0;
-        BigDecimal salesTaxRate = new BigDecimal("-0.1");
-        ContractAmount amount = new ContractAmount(amountIncludingTax, salesTaxRate);
-        log.info("salesTaxRate: {}", amount.getSalesTaxRate());
+        Mystery mystery = new Mystery();
+        mystery.calc(40, 30, 20, 10);
     }
 
     private static void p4() {
@@ -26,6 +26,13 @@ public class Book {
         Magic magic = new Magic(25);
 
         rpgService.cast(member, magic);
+    }
+
+    private static void p10() {
+        int amountIncludingTax = 0;
+        BigDecimal salesTaxRate = new BigDecimal("-0.1");
+        ContractAmount amount = new ContractAmount(amountIncludingTax, salesTaxRate);
+        log.info("salesTaxRate: {}", amount.getSalesTaxRate());
     }
 
 }
---
+++ corock/src/main/java/com/cowaine/corock/chapter02/game/Mystery.java
@@ -0,0 +1,14 @@
+package com.cowaine.corock.chapter02.game;
+
+public class Mystery {
+
+    public void calc(int p1, int p2, int d1, int d2) {
+        int d = 0;
+        d = p1 + p2;
+        d = d - ((d1 + d2) / 2);
+        if (d < 0) {
+            d = 0;
+        }
+    }
+
+}
