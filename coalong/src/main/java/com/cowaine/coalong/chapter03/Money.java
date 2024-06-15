4725d61d7ae124a6b750c1dd755ab6b30d49b9e5
coalong <minay0904@gmail.com>
2024-06-15 17:18:46 +0900

    [chapter03] 성숙한 클래스로 성장시키는 설계 기법 - 로우 데이터 객체 방지

--- coalong/src/main/java/com/cowaine/coalong/chapter03/Money.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter03/Money.java
@@ -5,4 +5,9 @@ import java.util.Currency;
 public class Money {
     int amount;         // 금액
     Currency currency;  // 통화 단위
+
+    public Money(int amount, Currency currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 }
