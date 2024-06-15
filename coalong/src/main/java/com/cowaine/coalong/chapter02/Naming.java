f5226d0dddc2585352945df51b0ea00778976dd6
coalong <minay0904@gmail.com>
2024-06-15 16:12:13 +0900

    [chapter02] 의도를 분명히 전달할 수 있는 이름 설계하기(improve)

--- coalong/src/main/java/com/cowaine/coalong/chapter02/Naming.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter02/Naming.java
@@ -2,12 +2,12 @@ package com.cowaine.coalong.chapter02;
 
 public class Naming {
 
-    void calcDamage(int p1, int p2, int d1, int d2) {
-        int d = 0;
-        d = p1 + p2;
-        d = d - ((d1 + d2) / 2);
-        if (d < 0) {
-            d = 0;
+    void calcDamage(int playerArmPower, int playerWeaponPower, int enemyBodyDefence, int enemyArmorDefence) {
+        int damageAmount = 0;
+        damageAmount = playerArmPower + playerWeaponPower;
+        damageAmount = damageAmount - ((enemyBodyDefence + enemyArmorDefence) / 2);
+        if (damageAmount < 0) {
+            damageAmount = 0;
         }
     }
 }
