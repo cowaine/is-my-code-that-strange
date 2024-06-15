0a93c06838624933e0a460ceb795e4fb337b7ce1
coalong <minay0904@gmail.com>
2024-06-15 17:35:58 +0900

    [chapter03] 불변 변수로 만들어서 예상하지 못한 동작 막기

--- coalong/src/main/java/com/cowaine/coalong/chapter03/Money.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter03/Money.java
@@ -3,8 +3,8 @@ package com.cowaine.coalong.chapter03;
 import java.util.Currency;
 
 public class Money {
-    int amount;         // 금액
-    Currency currency;  // 통화 단위
+    public final int amount;         // 금액
+    public final Currency currency;  // 통화 단위
 
     public Money(int amount, Currency currency) {
         if (amount < 0) {
@@ -19,7 +19,7 @@ public class Money {
         this.currency = currency;
     }
 
-    void add(int other) {
+    public void add(int other) {
         amount += other;
     }
 }
