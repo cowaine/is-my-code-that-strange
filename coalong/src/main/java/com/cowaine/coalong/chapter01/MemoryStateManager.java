4a8e1c8b167752527f0ef703bc3a563f4add6a6b
coalong <minay0904@gmail.com>
2024-06-15 14:38:37 +0900

    [chapter01] 기술 중심 명명 클래스 예시

---
+++ coalong/src/main/java/com/cowaine/coalong/chapter01/MemoryStateManager.java
@@ -0,0 +1,16 @@
+package com.cowaine.coalong.chapter01;
+
+public class MemoryStateManager {
+    int intValue01;
+
+    void changeIntValue01(int changeValue) {
+        intValue01 -= changeValue;
+        if(intValue01 < 0) {
+            intValue01 = 0;
+            updateState02Flg();
+        }
+    }
+
+    void updateState02Flg() {
+    }
+}
