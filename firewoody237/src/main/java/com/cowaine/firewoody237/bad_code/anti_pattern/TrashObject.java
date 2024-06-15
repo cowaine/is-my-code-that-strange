dfca16ff3e45a7c9d4aaeba097c23930fedd28b7
firewoody237 <firewoody237@gmail.com>
2024-06-15 11:06:40 +0900

    [1st Weeks] 1~3장 학습내용 - 의미를 알 수 없는 이름 - 분기 중첩 - 쓰레기 객체 - 데이터 클래스 - 클래스 설계

---
+++ firewoody237/src/main/java/com/cowaine/firewoody237/bad_code/anti_pattern/TrashObject.java
@@ -0,0 +1,11 @@
+package com.cowaine.firewoody237.bad_code.anti_pattern;
+
+import java.math.BigDecimal;
+
+public class TrashObject {
+    public class ConstractAmount {
+        // 비즈니스 로직에 대한 구현이 클래스 밖에서 발생할 수 있음 -> 문제 발생
+        public int amountIncludingTax;
+        public BigDecimal salesTaxRate;
+    }
+}
