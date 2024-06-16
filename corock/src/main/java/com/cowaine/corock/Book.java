c5bb8cdef996dbbdd3c3c6f089c36569839ce90a
cororing <corock.dev@gmail.com>
2024-06-16 23:10:02 +0900

    refactor: 히트 포인트 클래스 코드 리팩터링

--- corock/src/main/java/com/cowaine/corock/Book.java
+++ corock/src/main/java/com/cowaine/corock/Book.java
@@ -6,6 +6,7 @@ import com.cowaine.corock.chapter01.game.Member;
 import com.cowaine.corock.chapter01.game.RpgService;
 import com.cowaine.corock.chapter02.game.Damage;
 import com.cowaine.corock.chapter02.game.Game;
+import com.cowaine.corock.chapter02.game.HitPoint;
 import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
@@ -18,6 +19,15 @@ public class Book {
         // Book.p10();
         // Book.pp14To17();
         // Book.p18();
+
+        HitPoint hitPoint = new HitPoint(500);
+        log.info("Player's initial HP: {}", hitPoint.getValue());
+
+        HitPoint damagedHitPoint = hitPoint.damage(70);
+        log.info("Player's HP after taking damage: {}", damagedHitPoint.getValue());
+
+        HitPoint recoveredHitPoint = damagedHitPoint.recover(50);
+        log.info("Player's HP after using a health potion: {}", recoveredHitPoint.getValue());
     }
 
     private static void p4() {
--- corock/src/main/java/com/cowaine/corock/chapter02/game/HitPoint.java
+++ corock/src/main/java/com/cowaine/corock/chapter02/game/HitPoint.java
@@ -1,29 +1,59 @@
 package com.cowaine.corock.chapter02.game;
 
-// 히트포인트(HP)를 나타내는 클래스
-class HitPoint {
+import lombok.Getter;
+
+/**
+ * 히트 포인트(HP)를 나타내는 클래스다.
+ */
+@Getter
+public class HitPoint {
+
     private static final int MIN = 0;
     private static final int MAX = 999;
-    final int value;
 
-    HitPoint(final int value) {
-        if (value < MIN) throw new IllegalArgumentException(MIN + " 이상을 지정해 주세요.");
-        if (MAX < value) throw new IllegalArgumentException(MIN + " 이하를 지정해 주세요.");
+    private final int value;
+
+    /**
+     * 대미지 생성자다.
+     *
+     * @param value 대미지
+     */
+    public HitPoint(int value) {
+        if (value < MIN) {
+            throw new IllegalArgumentException(MIN + " 이상을 지정해 주세요.");
+        }
+
+        if (MAX < value) {
+            throw new IllegalArgumentException(MAX + " 이하를 지정해 주세요.");
+        }
 
         this.value = value;
     }
 
-    // 대미지를 받음.
-    HitPoint damage(final int damageAmount) {
-        final int damaged = value - damageAmount;
-        final int corrected = damaged < MIN ? MIN : damaged;
+    /**
+     * 대미지를 받는다.
+     *
+     * @param damageAmount 피해량
+     * @return 피해량을 반영한 히트 포인트
+     */
+    public HitPoint damage(int damageAmount) {
+        int damaged = value - damageAmount;
+        int corrected = Math.max(damaged, MIN);
+
         return new HitPoint(corrected);
     }
 
-    // 회복
-    HitPoint recover(final int recoveryAmount) {
-        final int recovered = value + recoveryAmount;
-        final int corrected = MAX < recovered ? MAX : recovered;
+    /**
+     * 대미지를 회복한다.
+     *
+     * @param recoveryAmount 회복량
+     * @return 회복량을 반영한 히트 포인트
+     */
+    public HitPoint recover(int recoveryAmount) {
+        int recovered = value + recoveryAmount;
+        int corrected = Math.min(MAX, recovered);
+
         return new HitPoint(corrected);
     }
+
 }
