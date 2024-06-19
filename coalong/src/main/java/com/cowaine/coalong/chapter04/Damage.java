5a597499655fac16a000478827573d69edf8b49b
coalong <minay0904@gmail.com>
2024-06-19 23:15:52 +0900

    [chapter04] 불변 변수로 만들어서 재할당 막기

--- coalong/src/main/java/com/cowaine/coalong/chapter04/Damage.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter04/Damage.java
@@ -3,14 +3,14 @@ package com.cowaine.coalong.chapter04;
 public class Damage {
     int damage(Member member, Enemy enemy) {
         // 멤버의 힘과 무기 성능을 기본 공격력으로 활용합니다.
-        int tmp = member.power() + member.weaponAttack();
+        final int basicAttackPower = member.power() + member.weaponAttack();
         // 멤버의 속도로 공격력을 보정합니다.
-        tmp = (int)(tmp * (1f + member.speed() / 100f));
+        final int finalAttackPower = (int)(basicAttackPower * (1f + member.speed() / 100f));
         // 공격력에서 적의 방어력을 뺀 값을 대미지로 사용합니다.
-        tmp = tmp - (int)(enemy.defence / 2);
+        final int reduction = (int)(enemy.defence / 2);
         // 대미지가 음수가 되지 않게 조정합니다.
-        tmp  = Math.max(0, tmp);
+        final int damage = Math.max(0, finalAttackPower - reduction);
 
-        return tmp;
+        return damage;
     }
 }
