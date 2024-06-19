7b8d0eb00bc706f732899569829b3c0a2820b27b
cororing <corock.dev@gmail.com>
2024-06-19 21:07:26 +0900

    refactor: 생성자로 확실하게 정상적인 값 설정하기 (생성자에서 유효성 검사하기)

--- corock/src/main/java/com/cowaine/corock/chapter03/Money.java
+++ corock/src/main/java/com/cowaine/corock/chapter03/Money.java
@@ -11,6 +11,14 @@ public class Money {
     private Currency currency;      // 통화 단위
 
     public Money(int amount, Currency currency) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("금액은 0 이상의 값을 지정해 주세요.");
+        }
+
+        if (currency == null) {
+            throw new NullPointerException("통화 단위를 지정해 주세요.");
+        }
+
         this.amount = amount;
         this.currency = currency;
     }
