a04c60fbbeeec124aad719cae9f9fd1c432e5406
coalong <minay0904@gmail.com>
2024-06-20 22:57:06 +0900

    [chapter04] 가변으로 인해 발생하는 의도하지 않은 영향 - 인스턴스 재사용하기

---
+++ coalong/src/main/java/com/cowaine/coalong/chapter04/ex02/Game.java
@@ -0,0 +1,7 @@
+package com.cowaine.coalong.chapter04.ex02;
+
+public class Game {
+    AttackPower attackPower = new AttackPower(20);
+    Weapon weaponA = new Weapon(attackPower);
+    Weapon weaponB = new Weapon(attackPower);
+}
