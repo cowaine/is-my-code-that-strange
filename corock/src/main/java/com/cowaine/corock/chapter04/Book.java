d9055bdc5707740f4b86bca7f6421931dbae8e53
cororing <corock.dev@gmail.com>
2024-06-20 12:24:37 +0900

    refactor: 코드 4.3 개별적인 불변 지역 변수를 사용하는 형태로 변경하기

--- corock/src/main/java/com/cowaine/corock/chapter04/Book.java
+++ corock/src/main/java/com/cowaine/corock/chapter04/Book.java
@@ -9,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 public class Book {
 
     public static void main(String[] args) {
-        log.info("tmp: {}", new Damage(new Member(), new Enemy(30)).damage());
+        log.info("damage: {}", new Damage(new Member(), new Enemy(30)).damage());
     }
 
 }
