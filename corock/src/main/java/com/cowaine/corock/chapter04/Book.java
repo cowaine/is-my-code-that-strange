f502897bcc51594decd029d0fc986ea26344995c
cororing <corock.dev@gmail.com>
2024-06-20 12:33:14 +0900

    feat: 클라이언트 사용 예시 추가

--- corock/src/main/java/com/cowaine/corock/chapter04/Book.java
+++ corock/src/main/java/com/cowaine/corock/chapter04/Book.java
@@ -3,12 +3,20 @@ package com.cowaine.corock.chapter04;
 import com.cowaine.corock.chapter04.game.Damage;
 import com.cowaine.corock.chapter04.game.Enemy;
 import com.cowaine.corock.chapter04.game.Member;
+import com.cowaine.corock.chapter04.product.Product;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Book {
 
     public static void main(String[] args) {
+        // Book.pp42To43();
+
+        Product product = new Product(39_800);
+        log.info("price: {}", product.addPrice(19_800));
+    }
+
+    private static void pp42To43() {
         log.info("damage: {}", new Damage(new Member(), new Enemy(30)).damage());
     }
 
--- corock/src/main/java/com/cowaine/corock/chapter04/product/Product.java
+++ corock/src/main/java/com/cowaine/corock/chapter04/product/Product.java
@@ -1,16 +1,21 @@
 package com.cowaine.corock.chapter04.product;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class Product {
 
     private static final int MAX_TOTAL_PRICE = 100_000;
 
-    private int totalPrice;
+    private final int totalPrice;
 
-    public void addPrice(final int productPrice) {
+    public int addPrice(final int productPrice) {
         final int increasedTotalPrice = totalPrice + productPrice;
         if (MAX_TOTAL_PRICE < increasedTotalPrice) {
             throw new IllegalArgumentException("구매 상한 금액을 넘었습니다.");
         }
+
+        return increasedTotalPrice;
     }
 
 }
