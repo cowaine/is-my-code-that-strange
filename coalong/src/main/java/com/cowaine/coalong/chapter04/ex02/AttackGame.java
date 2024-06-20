a466fdd1cd83209acf5d632e165272675d02f5b7
coalong <minay0904@gmail.com>
2024-06-20 23:29:01 +0900

    [chapter04] 가변으로 인해 발생하는 의도하지 않은 영향 - 공격력을 강화하는 처리

--- coalong/src/main/java/com/cowaine/coalong/chapter04/ex02/AttackGame.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter04/ex02/AttackGame.java
@@ -2,9 +2,10 @@ package com.cowaine.coalong.chapter04.ex02;
 
 public class AttackGame {
     public static void main(String[] args) {
-        Attack();
+        attack();
+        action();
     }
-    static void Attack() {
+    static void attack() {
         AttackPower attackPowerA = new AttackPower(20);
         AttackPower attackPowerB = new AttackPower(20);
 
@@ -15,5 +16,12 @@ public class AttackGame {
 
         System.out.println("Weapon A attack power : " + weaponA.attackPower.value);
         System.out.println("Weapon B attack power : " + weaponB.attackPower.value);
+    }
+
+    static void action() {
+        AttackPower attackPower = new AttackPower(20);
+
+        attackPower.reinforce(15);
+        System.out.println("attack power : " + attackPower.value);
     }
 }
