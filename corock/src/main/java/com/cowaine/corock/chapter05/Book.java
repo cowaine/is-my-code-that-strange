96706b6ac4c9989d5f1c1225f5f451f2782c0d44
cororing <corock.dev@gmail.com>
2024-06-21 18:52:10 +0900

    refactor: 인스턴스 변수를 사용하는 구조로 변경하기

--- corock/src/main/java/com/cowaine/corock/chapter05/Book.java
+++ corock/src/main/java/com/cowaine/corock/chapter05/Book.java
@@ -1,7 +1,6 @@
 package com.cowaine.corock.chapter05;
 
 import com.cowaine.corock.chapter05.order.Money;
-import com.cowaine.corock.chapter05.order.OrderManager;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -11,7 +10,7 @@ public class Book {
         Money moneyData1 = new Money(100);
         Money moneyData2 = new Money(500);
 
-        log.info("money: {}", new Money(OrderManager.add(moneyData1.getAmount(), moneyData2.getAmount())).getAmount());
+        log.info("money: {}", moneyData1.add(moneyData2).getAmount());
     }
 
 }
--- corock/src/main/java/com/cowaine/corock/chapter05/order/OrderManager.java
+++ corock/src/main/java/com/cowaine/corock/chapter05/order/OrderManager.java
@@ -1,5 +1,6 @@
 package com.cowaine.corock.chapter05.order;
 
+@Deprecated(forRemoval = true)
 public class OrderManager {
 
     public static int add(int moneyAmount1, int moneyAmount2) {
