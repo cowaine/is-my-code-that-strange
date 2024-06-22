9b030fb9e74ae513bdbbbc05e3ebf6e92d315400
coalong <minay0904@gmail.com>
2024-06-22 19:08:03 +0900

    [Chapter05] Equipment enum 인 경우

--- coalong/src/main/java/com/cowaine/coalong/chapter05/game/Equipment.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter05/game/Equipment.java
@@ -1,13 +1,20 @@
 package com.cowaine.coalong.chapter05.game;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class Equipment {
-    public static final Equipment EMPTY = new Equipment(null, 0, 0, 0);
+@Getter
+public enum Equipment {
 
-    final String name;
-    final int price;
-    final int defence;
-    final int magicDefence;
+    EMPTY("", 0, 0, 0),
+    HEAD("머리", 500000, 500, 500),
+    ARMOR("갑옷", 1000, 10000, 1000),
+    ARM("팔", 5000, 1000, 1000);
+
+    private final String name;
+    private final int price;
+    private final int defence;
+    private final int magicDefence;
+
 }
