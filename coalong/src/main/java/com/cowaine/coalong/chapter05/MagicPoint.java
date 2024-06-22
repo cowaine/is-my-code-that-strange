aced651f30fffbd64b3663ba8b098c29f82e0ee1
coalong <minay0904@gmail.com>
2024-06-22 17:43:45 +0900

    [chapter05] 의미 있는 단위는 모두 클래스로 만들기 - 매직포인트와 관련된 로직 캡슐화

--- coalong/src/main/java/com/cowaine/coalong/chapter05/MagicPoint.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter05/MagicPoint.java
@@ -7,10 +7,43 @@ import java.util.List;
  */
 public class MagicPoint {
     // 현재 잔량
-    int currentAmount;
+    private int currentAmount;
     // 원래 최댓값
-    int originalMaxAmount;
+    private int originalMaxAmount;
     // 장비 착용에 따른 최댓값 증가량
-    List<Integer> maxIncrements;
+    private List<Integer> maxIncrements;
 
+    /**
+     * @return 현재 매직포인트 잔량
+     */
+    int current() {
+        return currentAmount;
+    }
+
+    /**
+     * @return 매직포인트 최닷값
+     */
+    int max() {
+        int amount = maxIncrements.get(currentAmount);
+        for (int each : maxIncrements) {
+            amount -= each;
+        }
+        return amount;
+    }
+
+    /**
+     * 매직포인트 회복하기
+     * @param recoveryAmount 회복량
+     */
+    void recover(final int recoveryAmount) {
+        currentAmount = Math.min(this.currentAmount + recoveryAmount, max());
+    }
+
+    /**
+     * 매직포인트 소비하기
+     * @param consumeAmount 소비량
+     */
+    void consume(final int consumeAmount) {
+        //TODO
+    }
 }
