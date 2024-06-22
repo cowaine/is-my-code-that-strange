d990abbfceb2aa084d3d5843d61c466ea7cae91a
coalong <minay0904@gmail.com>
2024-06-22 17:37:56 +0900

    [chapter05] 의미 있는 단위는 모두 클래스로 만들기 - 매개변수가 아니라 인스턴스 변수로 표현하기

--- coalong/src/main/java/com/cowaine/coalong/chapter05/MagicPoint.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter05/MagicPoint.java
@@ -2,27 +2,15 @@ package com.cowaine.coalong.chapter05;
 
 import java.util.List;
 
+/**
+ * 매직포인트
+ */
 public class MagicPoint {
+    // 현재 잔량
+    int currentAmount;
+    // 원래 최댓값
+    int originalMaxAmount;
+    // 장비 착용에 따른 최댓값 증가량
+    List<Integer> maxIncrements;
 
-    /**
-     * 매직포인트 회복하기
-     *
-     * @param currentMagicPoint       현재 매직포인트 잔량
-     * @param originalMagicPoint      원래 매직포인트 최댓값
-     * @param maxMagicPointIncrements 장비로 증가하는 매직포인트 최댓값 증가량
-     * @param recoveryMagicPoint      회복량
-     * @return 회복 후의 매직포인트 잔량
-     */
-    int recoverMagicPoint(int currentMagicPoint,
-                          int originalMagicPoint,
-                          List<Integer> maxMagicPointIncrements,
-                          int recoveryMagicPoint) {
-
-        int currentMaxMagicPoint = originalMagicPoint;
-        for (int each : maxMagicPointIncrements) {
-            currentMaxMagicPoint += each;
-        }
-
-        return Math.min(currentMagicPoint + recoveryMagicPoint, currentMagicPoint);
-    }
 }
