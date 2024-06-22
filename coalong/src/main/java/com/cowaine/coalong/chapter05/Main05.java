7664a47d628527afb990faf7b3f6d8be0a5027e7
coalong <minay0904@gmail.com>
2024-06-22 15:24:19 +0900

    [chapter05] static 메서드 오용

---
+++ coalong/src/main/java/com/cowaine/coalong/chapter05/Main05.java
@@ -0,0 +1,17 @@
+package com.cowaine.coalong.chapter05;
+
+public class Main05 {
+    public static void main(String[] args) {
+        page61();
+    }
+
+    private static void page61() {
+
+        // moneyData1, moneyData2 는 데이터 클래스
+        MoneyData moneyData1 = new MoneyData();
+        MoneyData moneyData2 = new MoneyData();
+
+        moneyData1.amount = OrderManager.add(moneyData1.amount, moneyData2.amount);
+        System.out.println(moneyData1.amount);
+    }
+}
