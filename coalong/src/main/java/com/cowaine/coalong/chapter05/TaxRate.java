d5b542b25194aaeec062797d34e499300e389226
coalong <minay0904@gmail.com>
2024-06-22 16:36:08 +0900

    [chapter05] 객체 지향 설계의 기본으로 돌아가기

---
+++ coalong/src/main/java/com/cowaine/coalong/chapter05/TaxRate.java
@@ -0,0 +1,10 @@
+package com.cowaine.coalong.chapter05;
+
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
+public class TaxRate {
+    final BigDecimal value;
+}
