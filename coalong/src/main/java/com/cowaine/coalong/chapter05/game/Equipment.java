31544bccfd1471c97c58ef50940542e63575ed29
coalong <minay0904@gmail.com>
2024-06-22 18:56:19 +0900

    [Chapter05] Equipment 클래스인 경우

--- coalong/src/main/java/com/cowaine/coalong/chapter05/game/Equipment.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter05/game/Equipment.java
@@ -1,5 +1,13 @@
 package com.cowaine.coalong.chapter05.game;
 
-public enum Equipment {
-    EMPTY
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Equipment {
+    public static final Equipment EMPTY = new Equipment(null, 0, 0, 0);
+
+    final String name;
+    final int price;
+    final int defence;
+    final int magicDefence;
 }
