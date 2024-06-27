3ae864069a9223b336d9e7c54eb2a22591a69c22
coalong <minay0904@gmail.com>
2024-06-27 23:17:20 +0900

    [chapter06] 조기 리턴으로 중첩 제거하기

--- coalong/src/main/java/com/cowaine/coalong/chapter06/RpgGame.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter06/RpgGame.java
@@ -2,16 +2,13 @@ package com.cowaine.coalong.chapter06;
 
 public class RpgGame {
     void enableMagic(Member member, Magic magic) {
-        // 살아 있는가
-        if (0 < member.hitPoint) {
-            // 움직일 수 있는가
-            if (member.canAct()) {
-                // 매직포인트가 남아 있는가
-                if (magic.costMaicPoint <= member.magicPoint) {
-                    member.consumeMagicPoint(magic.costMaicPoint);
-                    member.chant(magic);
-                }
-            }
-        }
+        // 살아 있지 않은 경우 리턴하므로 처리를 종료합니다.
+        // 조기 리턴으로 변경하기 위해 조건을 반전했습니다.
+        if (member.hitPoint <= 0) return;
+        if (!member.canAct()) return;
+        if (member.magicPoint < magic.costMaicPoint) return;
+
+        member.consumeMagicPoint(magic.costMaicPoint);
+        member.chant(magic);
     }
 }
