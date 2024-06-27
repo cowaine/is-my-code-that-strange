d430fffdba8a9c56f40af81827ec111a224f9e2e
coalong <minay0904@gmail.com>
2024-06-27 23:42:52 +0900

    [chapter06] else 절을ㅇ 모두 제거하여 개선한 코드

--- coalong/src/main/java/com/cowaine/coalong/chapter06/HealthCheck.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter06/HealthCheck.java
@@ -4,14 +4,10 @@ public class HealthCheck {
     HealthCondition checkHealth(Member member) {
         float hitPointRate = (float) member.hitPoint / member.maxHitPoint;
 
-        if (hitPointRate == 0) {
-            return HealthCondition.DEAD;
-        } else if (hitPointRate < 0.3) {
-            return HealthCondition.DANGER;
-        } else if (hitPointRate < 0.5) {
-            return HealthCondition.CAUTION;
-        } else {
-            return HealthCondition.FINE;
-        }
+        if (hitPointRate == 0) return HealthCondition.DEAD;
+        if (hitPointRate < 0.3) return HealthCondition.DANGER;
+        if (hitPointRate < 0.5) return HealthCondition.CAUTION;
+
+        return HealthCondition.FINE;
     }
 }
