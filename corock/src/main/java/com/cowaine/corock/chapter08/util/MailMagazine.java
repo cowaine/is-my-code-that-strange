3491a0c40f9bcaa55cbabeb43bf73169316f5a81
cororing <corock.dev@gmail.com>
2024-06-30 17:00:43 +0900

    feat: 인스턴스 변수별로 클래스 분할을 하지 않은 유틸리티 클래스 추가

---
+++ corock/src/main/java/com/cowaine/corock/chapter08/util/MailMagazine.java
@@ -0,0 +1,4 @@
+package com.cowaine.corock.chapter08.util;
+
+public class MailMagazine {
+}
---
+++ corock/src/main/java/com/cowaine/corock/chapter08/util/Utils.java
@@ -0,0 +1,26 @@
+package com.cowaine.corock.chapter08.util;
+
+public class Utils {
+
+    // 상품 예약 ID
+    private int reservationId;
+
+    // 화면 표시 설정
+    private ViewSettings viewSettings;
+
+    // 메일 매거진
+    private MailMagazine mailMagazine;
+
+    void cancelReservation() {
+        // reservationId 를 사용한 예약 취소 처리
+    }
+
+    void darkMode() {
+        // viewSettings 를 사용한 다크 모드 표시 전환 처리
+    }
+
+    void beginSendMail() {
+        // mailMagazine 을 사용한 메일 전송 처리
+    }
+
+}
