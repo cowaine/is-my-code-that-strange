69c9f8ade0b3916c85b6305ec0b9cc4d6f9afc56
cororing <corock.dev@gmail.com>
2024-06-30 14:30:05 +0900

    refactor: 조기 continue 로 조건 분기 중첩 제거

--- corock/src/main/java/com/cowaine/corock/chapter07/game/MemberService.java
+++ corock/src/main/java/com/cowaine/corock/chapter07/game/MemberService.java
@@ -13,17 +13,27 @@ public class MemberService {
 
     public void reduceHitPointForPoisonedMembers() {
         for (Member member : members) {
-            if (0 < member.getHitPoint()) {
-                if (member.containsState(StateType.POISON)) {
-                    member.setHitPoint(10);
-
-                    if (member.getHitPoint() <= 0) {
-                        member.setHitPoint(0);
-                        member.addState(StateType.DEAD);
-                        member.removeState(StateType.POISON);
-                    }
-                }
+            /*
+             * 살아 있지 않다면 continue 로 다음 반복으로 넘어감.
+             * 조기 continue 로 변경하려면 조건을 반전해야 함.
+             */
+            if (member.getHitPoint() == 0) {
+                continue;
             }
+
+            if (!member.containsState(StateType.POISON)) {
+                continue;
+            }
+
+            member.setHitPoint(10);
+
+            if (0 < member.getHitPoint()) {
+                continue;
+            }
+
+            member.setHitPoint(0);
+            member.addState(StateType.DEAD);
+            member.removeState(StateType.POISON);
         }
     }
 
