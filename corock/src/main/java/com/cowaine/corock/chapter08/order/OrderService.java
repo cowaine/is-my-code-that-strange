b68a4537c719a923500e9be80d407c63306f34d4
cororing <corock.dev@gmail.com>
2024-06-30 17:38:57 +0900

    feat: private 메서드가 너무 많다는 것은 책임이 너무 많다는 것

---
+++ corock/src/main/java/com/cowaine/corock/chapter08/order/OrderService.java
@@ -0,0 +1,21 @@
+package com.cowaine.corock.chapter08.order;
+
+import com.cowaine.corock.chapter08.discount.Product;
+
+import java.util.List;
+
+class OrderService {
+
+    // (...)
+
+    private int calcDiscountPrice(int price) {
+        // 할인 가격 계산 로직
+        return 0;
+    }
+
+    private List<Product> getProductBrowsingHistory(int userId) {
+        // 최근 본 상품 리스트를 확인하는 로직
+        return null;
+    }
+
+}
