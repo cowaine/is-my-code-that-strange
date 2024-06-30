8123380bd1292203c0e30efed8873d6635204566
cororing <corock.dev@gmail.com>
2024-06-30 17:41:07 +0900

    refactor: 책임이 다른 메서드는 다른 클래스로 분리

--- corock/src/main/java/com/cowaine/corock/chapter08/order/OrderService.java
+++ corock/src/main/java/com/cowaine/corock/chapter08/order/OrderService.java
@@ -8,11 +8,13 @@ class OrderService {
 
     // (...)
 
+    @Deprecated(forRemoval = true)
     private int calcDiscountPrice(int price) {
         // 할인 가격 계산 로직
         return 0;
     }
 
+    @Deprecated(forRemoval = true)
     private List<Product> getProductBrowsingHistory(int userId) {
         // 최근 본 상품 리스트를 확인하는 로직
         return null;
---
+++ corock/src/main/java/com/cowaine/corock/chapter08/product/ProductBrowsingHistory.java
@@ -0,0 +1,14 @@
+package com.cowaine.corock.chapter08.product;
+
+import com.cowaine.corock.chapter08.discount.Product;
+
+import java.util.List;
+
+public class ProductBrowsingHistory {
+
+    public List<Product> get(int userId) {
+        // 최근 본 상품 리스트를 확인하는 로직
+        return null;
+    }
+
+}
