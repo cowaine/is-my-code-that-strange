6d4d73edd917fbfac76dc5e08694eeff0345b4bd
coalong <minay0904@gmail.com>
2024-07-05 21:45:52 +0900

    [Chapter07] 조기 컨티뉴로 조건 분기 중첩 제거하기

--- coalong/src/main/java/com/cowaine/coalong/chapter07/Chap7.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter07/Chap7.java
@@ -14,15 +14,15 @@ public class Chap7 {
             // 살아 있지 않다면 continue 로 다음 반복으로 넘어감.
             // 조기 continue 로 변경하려면 조건을 반전해야 함.
             if (member.hitPoint == 0) continue;
+            if (!member.containsState(StateType.poison)) continue;
 
-            if (member.containsState(StateType.poison)) {
-                member.hitPoint -= 10;
-                if (member.hitPoint <= 0) {
-                    member.hitPoint = 0;
-                    member.addState(StateType.dead);
-                    member.removeState(StateType.poison);
-                }
-            }
+            member.hitPoint -= 10;
+
+            if (0 < member.hitPoint) continue;
+
+            member.hitPoint = 0;
+            member.addState(StateType.dead);
+            member.removeState(StateType.poison);
         }
     }
 }
