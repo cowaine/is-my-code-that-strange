af2961abe56ee0391841e5795c531aad790928f0
bunsung92 <bunsung92@naver.com>
2024-07-06 17:27:05 +0900

    feat: 7.1.1 감옥 열쇠를 소지하고 있는지 확인하는 코드

---
+++ irisation23/src/main/java/com/cowaine/seventh/one/이미_존재하는_기능을_다시_구현하지_말기.java
@@ -0,0 +1,23 @@
+package com.cowaine.seventh.one;
+
+import com.cowaine.seventh.domain.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 이미_존재하는_기능을_다시_구현하지_말기 {
+    public static void main(String[] args) {
+        // 7.1 '감옥 열쇠'를 소지하고 있는지 확인하는 코드
+        boolean hasPrisonKey = false;
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("일반 열쇠"));
+        items.add(new Item("감옥 열쇠"));
+
+        for (Item item : items) {
+            if (item.getName().equals("감옥 열쇠")) {
+                hasPrisonKey = true;
+                break;
+            }
+        }
+    }
+}
