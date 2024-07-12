86674be3f9ac1cc063045142efe19d110c5ab44c
coalong <minay0904@gmail.com>
2024-07-12 19:31:29 +0900

    [Chapter09] null 로 만들었으므로, null 인지 체크해야함

--- coalong/src/main/java/com/cowaine/coalong/chapter09/Member.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter09/Member.java
@@ -11,12 +11,19 @@ public class Member {
     // 방어구의 방어력과 캐릭터의 방어력을 합산해서 리턴
     int totalDefense() {
         int total = defense;
-        total += head.getDefence();
-        total += body.getDefence();
-        total += arm.getDefence();
+        if (head != null) {
+            total += head.getDefence();
+        }
+        if (body != null) {
+            total += body.getDefence();
+        }
+        if (arm != null) {
+            total += arm.getDefence();
+        }
         return total;
     }
 
+    // 장착하지 않은 상태를 null 로 표현하기
     void takeOffAllEquipment() {
         head = null;
         body = null;
