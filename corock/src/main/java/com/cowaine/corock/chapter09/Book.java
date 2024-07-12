a3b10496fb440693fbfdee9a671288019e88cbb5
cororing <corock.dev@gmail.com>
2024-07-12 16:28:43 +0900

    feat: 전역 변수 예시 추가

--- corock/src/main/java/com/cowaine/corock/chapter09/Book.java
+++ corock/src/main/java/com/cowaine/corock/chapter09/Book.java
@@ -3,6 +3,10 @@ package com.cowaine.corock.chapter09;
 public class Book {
 
     public static void main(String[] args) {
+        // Book.p191();
+    }
+
+    private static void p191() {
         // 레이블 문자열, 표시 색(RGB), 최대 문자 수
         String title = "타이틀,255,250,240,64";
     }
