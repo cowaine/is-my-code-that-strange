91625ac03998f2338695b737ea0eae61ea858241
coalong <minay0904@gmail.com>
2024-07-12 20:30:50 +0900

    [Chapter09] 문제가 발생했다면 소리치기

--- coalong/src/main/java/com/cowaine/coalong/chapter09/ProductReservationService.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter09/ProductReservationService.java
@@ -10,7 +10,19 @@ public class ProductReservationService {
     void reserve(Reservation reservation) {
         try {
             reservation.add(product);
-        } catch (Exception e) {
+        } catch (IllegalArgumentException e) {
+            // 오류를 보고하고, 로그에 기록
+            reportError(e);
+            // 상위 레이어에 오류와 관련된 통지 요구
+            requestNotifyError("예약할 수 없는 상품입니다.");
         }
+    }
+
+    private void requestNotifyError(String s) {
+        // TODO
+    }
+
+    private void reportError(IllegalArgumentException e) {
+        // TODO
     }
 }
