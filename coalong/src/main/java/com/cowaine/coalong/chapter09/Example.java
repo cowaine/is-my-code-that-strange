7fc77a8959246421e073319e8138a87da470e6b3
coalong <minay0904@gmail.com>
2024-07-12 20:43:49 +0900

    [Chapter09] 리플렉션으로 인한 클래스 구조와 값 변경 문제

--- coalong/src/main/java/com/cowaine/coalong/chapter09/Example.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter09/Example.java
@@ -1,7 +1,20 @@
 package com.cowaine.coalong.chapter09;
 
+import java.lang.reflect.Field;
+
 public class Example {
     // 레이블 문자열, 표시 색(RGB), 최대 문자 수
     String title = "타이틀, 255, 250, 240, 64";
+
+    void show9dot17() throws IllegalAccessException, NoSuchFieldException {
+        Level level = Level.initialize();
+        System.out.println("Level : " + level.value);
+
+        Field field = Level.class.getDeclaredField("value");
+        field.setAccessible(true);
+        field.setInt(level, 999);
+
+        System.out.println("Level : " + level.value);
+    }
 
 }
