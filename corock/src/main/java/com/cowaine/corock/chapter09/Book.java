3258c0017a72e72370facf04a731e982f820e418
cororing <corock.dev@gmail.com>
2024-07-12 17:13:07 +0900

    feat: 자료형의 장점을 살리지 못하는 하드 코딩

--- corock/src/main/java/com/cowaine/corock/chapter09/Book.java
+++ corock/src/main/java/com/cowaine/corock/chapter09/Book.java
@@ -1,26 +1,29 @@
 package com.cowaine.corock.chapter09;
 
+import com.cowaine.corock.chapter09.customer.User;
 import com.cowaine.corock.chapter09.game.Level;
 import com.cowaine.corock.chapter09.game.Member;
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
 @Slf4j
 public class Book {
 
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, ClassNotFoundException,
+            InvocationTargetException, NoSuchMethodException, InstantiationException {
+
         // Book.p191();
         // Book.pp194To197();
+        // Book.p201();
 
-        Level level = Level.initialize();
-        log.info("Level: {}", level.getValue());
+        String packageName = "com.cowaine.corock.chapter09.customer";
+        String className = "User";
+        User user = (User) generateInstance(packageName, className);
 
-        Field field = Level.class.getDeclaredField("value");
-        field.setAccessible(true);
-        field.setInt(level, 999);
-
-        log.info("Level: {}", level.getValue());
+        log.info("User: {}", user);
     }
 
     private static void p191() {
@@ -31,6 +34,39 @@ public class Book {
     private static void pp194To197() {
         Member member = new Member();
         member.showBodyEquipment();
+    }
+
+    private static void p201() throws NoSuchFieldException, IllegalAccessException {
+        Level level = Level.initialize();
+        log.info("Level: {}", level.getValue());
+
+        Field field = Level.class.getDeclaredField("value");
+        field.setAccessible(true);
+        field.setInt(level, 999);
+
+        log.info("Level: {}", level.getValue());
+    }
+
+    /**
+     * 클래스 이름을 지정해서 인스턴스를 생성한다.
+     *
+     * @param packageName 패키지 이름
+     * @param className   인스턴스를 생성할 클래스 이름
+     * @return 지정한 클래스의 인스턴스
+     * @throws ClassNotFoundException
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     */
+    static Object generateInstance(String packageName, String className) throws ClassNotFoundException,
+            NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        String fillName = packageName + "." + className;
+        Class<?> clazz = Class.forName(fillName);
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+
+        return constructor.newInstance();
     }
 
 }
