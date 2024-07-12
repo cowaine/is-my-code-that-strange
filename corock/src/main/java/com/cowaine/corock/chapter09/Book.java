61babe2f0b719e0bc8d7d3a20070dd5318db86b3
cororing <corock.dev@gmail.com>
2024-07-12 16:26:48 +0900

    feat: 문자열 자료형에 대한 집착

---
+++ corock/src/main/java/com/cowaine/corock/chapter09/Book.java
@@ -0,0 +1,10 @@
+package com.cowaine.corock.chapter09;
+
+public class Book {
+
+    public static void main(String[] args) {
+        // 레이블 문자열, 표시 색(RGB), 최대 문자 수
+        String title = "타이틀,255,250,240,64";
+    }
+
+}
