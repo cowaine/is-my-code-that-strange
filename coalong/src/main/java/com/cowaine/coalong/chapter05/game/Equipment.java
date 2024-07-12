1d101fe98ae636a71de0742075b6aae6afd31f73
coalong <minay0904@gmail.com>
2024-07-12 19:57:47 +0900

    [Chapter09] "장비하지 않음"을 null 이 아닌 방법으로 표현하기

--- coalong/src/main/java/com/cowaine/coalong/chapter05/game/Equipment.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter05/game/Equipment.java
@@ -1,13 +1,11 @@
 package com.cowaine.coalong.chapter05.game;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public enum Equipment {
 
-    EMPTY("", 0, 0, 0),
+    EMPTY("장비없음", 0, 0, 0),
     HEAD("머리", 500000, 500, 500),
     ARMOR("갑옷", 1000, 10000, 1000),
     ARM("팔", 5000, 1000, 1000);
@@ -17,4 +15,13 @@ public enum Equipment {
     private final int defence;
     private final int magicDefence;
 
+    Equipment(String name, int price, int defence, int magicDefence) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("잘못된 이름입니다.");
+        }
+        this.name = name;
+        this.price = price;
+        this.defence = defence;
+        this.magicDefence = magicDefence;
+    }
 }
