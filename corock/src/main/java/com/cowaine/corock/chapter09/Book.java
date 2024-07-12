bd7914ffedba4d14aee6d4742cbf5f7554fe373b
cororing <corock.dev@gmail.com>
2024-07-12 17:05:51 +0900

    feat: 리플렉션으로 인한 클래스 구조와 값 변경 문제

--- corock/src/main/java/com/cowaine/corock/chapter09/Book.java
+++ corock/src/main/java/com/cowaine/corock/chapter09/Book.java
@@ -1,19 +1,36 @@
 package com.cowaine.corock.chapter09;
 
+import com.cowaine.corock.chapter09.game.Level;
 import com.cowaine.corock.chapter09.game.Member;
+import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Field;
+
+@Slf4j
 public class Book {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         // Book.p191();
+        // Book.pp194To197();
 
-        Member member = new Member();
-        member.showBodyEquipment();
+        Level level = Level.initialize();
+        log.info("Level: {}", level.getValue());
+
+        Field field = Level.class.getDeclaredField("value");
+        field.setAccessible(true);
+        field.setInt(level, 999);
+
+        log.info("Level: {}", level.getValue());
     }
 
     private static void p191() {
         // 레이블 문자열, 표시 색(RGB), 최대 문자 수
         String title = "타이틀,255,250,240,64";
+    }
+
+    private static void pp194To197() {
+        Member member = new Member();
+        member.showBodyEquipment();
     }
 
 }
