80c6b02fd1129979d8cf880d7b4b78084dcca2c4
cororing <corock.dev@gmail.com>
2024-07-14 17:04:09 +0900

    feat: 이용 약관 읽어 보기

---
+++ corock/src/main/java/com/cowaine/corock/chapter10/amount/SalesCommissionRate.java
@@ -0,0 +1,10 @@
+package com.cowaine.corock.chapter10.amount;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class SalesCommissionRate {
+
+    final double value;
+
+}
---
+++ corock/src/main/java/com/cowaine/corock/chapter10/amount/SalesPrice.java
@@ -0,0 +1,10 @@
+package com.cowaine.corock.chapter10.amount;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class SalesPrice {
+
+    final int amount;
+
+}
