1268d71faedd03980f0a38cb138d5f8f6422386a
coalong <minay0904@gmail.com>
2024-07-20 13:15:30 +0900

    [chapter10] 가능하면 메서드의 이름은 동사 하나로 구성되게 하기

---
+++ coalong/src/main/java/com/cowaine/coalong/chpater10/PartyItems.java
@@ -0,0 +1,28 @@
+package com.cowaine.coalong.chpater10;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PartyItems extends Item {
+    static final int MAX_ITEM_COUNT = 99;
+    final List<Item> items;
+
+    PartyItems() {
+        items = new ArrayList<>();
+    }
+
+    private PartyItems(final List<Item> items) {
+        this.items = items;
+    }
+
+    PartyItems add(final PartyItems newItem) {
+        if (items.size() == MAX_ITEM_COUNT) {
+            throw new RuntimeException("Too many items");
+        }
+
+        final List<Item> adding = new ArrayList<>(items);
+        adding.add(newItem);
+        return new PartyItems(adding);
+    }
+}
