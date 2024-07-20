52b0085f99025dbf11672eb3b441e6199b799fcb
coalong <minay0904@gmail.com>
2024-07-20 12:36:17 +0900

    [chapter10] 구조에 악영향을 미치는 이름

---
+++ coalong/src/main/java/com/cowaine/coalong/chpater10/MemberManager.java
@@ -0,0 +1,33 @@
+package com.cowaine.coalong.chpater10;
+
+public class MemberManager {
+    // 멤버의 히트포인트 추출하기
+    int getHitPoint(int memberId) {
+        return 0;
+    }
+
+    // 멤버의 매직포인트 추출하기
+    int getMagicPoint(int memberId) {
+        return 0;
+    }
+
+    // 멤버 보행 애니메이션 시작하기
+    void startWalkAnimation(int memberId) {
+        // TODO
+    }
+
+    // 멤버의 능력치를 CSV 형식으로 내보내기
+    void exportParamsToCsv() {
+        // TODO
+    }
+
+    // 적이 생존해 있는지 확인히가
+    boolean enemyIsAlive(int enemyId) {
+        return false;
+    }
+
+    // BGM 재생하기
+    void playBgm(String bgmName) {
+        // TODO
+    }
+}
