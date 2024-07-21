1448d3ee34aaeed0fc63aba2d71c3c9dfd837924
coalong <minay0904@gmail.com>
2024-07-21 18:38:26 +0900

    [chapter12] 커맨드/쿼리 분리 - 상태 변경과 추출을 동시에 실행하는 메서드

---
+++ coalong/src/main/java/com/cowaine/coalong/chapter12/PointService.java
@@ -0,0 +1,11 @@
+package com.cowaine.coalong.chapter12;
+
+public class PointService {
+
+    private int point;
+
+    int gainAndGetPoint() {
+        point += 10;
+        return point;
+    }
+}
