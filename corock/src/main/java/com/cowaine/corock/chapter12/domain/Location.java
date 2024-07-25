fe047c4cdcddde970d61ac4b007d07a9b9bf8b1f
cororing <corock.dev@gmail.com>
2024-07-25 17:43:36 +0900

    refactor: 오류는 리턴 값으로 리턴하지 말고 예외 발생시키기

--- corock/src/main/java/com/cowaine/corock/chapter12/domain/Location.java
+++ corock/src/main/java/com/cowaine/corock/chapter12/domain/Location.java
@@ -1,14 +1,19 @@
 package com.cowaine.corock.chapter12.domain;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-
 // 위치를 나타내는 클래스
-@AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class Location {
 
     private final int x;
     private final int y;
+
+    Location(final int x, final int y) {
+        if (!valid(x, y)) {
+            throw new IllegalArgumentException("잘못된 위치입니다.");
+        }
+
+        this.x = x;
+        this.y = y;
+    }
 
     // (...)
 
@@ -16,11 +21,8 @@ public class Location {
     Location shift(final int shiftX, final int shiftY) {
         int nextX = x + shiftX;
         int nextY = y + shiftY;
-        if (valid(nextX, nextY)) {
-            return new Location(nextX, nextY);
-        }
-        // (-1. -1)은 오류 값
-        return new Location(-1, -1);
+
+        return new Location(nextX, nextY);
     }
 
     private boolean valid(int nextX, int nextY) {
