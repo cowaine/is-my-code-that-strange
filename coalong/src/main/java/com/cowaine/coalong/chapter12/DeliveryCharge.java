275c708d949d8323611bafa309ce330e070f52e3
coalong <minay0904@gmail.com>
2024-07-25 21:38:52 +0900

    [chapter12] 값 전달 실수

---
+++ coalong/src/main/java/com/cowaine/coalong/chapter12/DeliveryCharge.java
@@ -0,0 +1,8 @@
+package com.cowaine.coalong.chapter12;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class DeliveryCharge {
+    private final int deliveryPrice;
+}
--- coalong/src/main/java/com/cowaine/coalong/chapter12/PriceService.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter12/PriceService.java
@@ -5,6 +5,9 @@ public class PriceService {
         int price = productPrice.add(otherPrice); // 상품 가격 합계
         int discountedPrice = calcDiscountedPrice(price); // 할인 금액
         int deliveryPrice = calcDeliveryPrice(discountedPrice); // 배송비
+
+        // 배송 수수료 DeliveryCharge 에는 배송비가 전달되어야 하는데 상품 가격 합계가 전달되고 있음.
+        DeliveryCharge deliveryCharge = new DeliveryCharge(price); //
     }
 
     private int calcDeliveryPrice(int discountedPrice) {
