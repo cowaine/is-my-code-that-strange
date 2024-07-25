a90db0f5db92e6b65a3f80ede4362d72f8fe5694
cororing <corock.dev@gmail.com>
2024-07-25 17:28:53 +0900

    refactor: 커맨드와 쿼리를 각각의 메서드로 분리

--- corock/src/main/java/com/cowaine/corock/chapter12/domain/Point.java
+++ corock/src/main/java/com/cowaine/corock/chapter12/domain/Point.java
@@ -4,8 +4,23 @@ public class Point {
 
     private int point;
 
+    @Deprecated(forRemoval = true)
     int gainAndGetPoint() {
         point += 10;
+        return point;
+    }
+
+    /**
+     * 포인트를 증가(커맨드)
+     */
+    void gainPoint() {
+        point += 10;
+    }
+
+    /**
+     * 포인트를 반환(쿼리)
+     */
+    int getPoint() {
         return point;
     }
 
