7ac4f46a58baac90687ae17ade3c83b864cbbe6c
coalong <minay0904@gmail.com>
2024-07-25 21:55:30 +0900

    [chapter12] 오류는 리턴 값으로 리턴하지 말고 예외 발생시키기

--- coalong/src/main/java/com/cowaine/coalong/chapter12/Location.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter12/Location.java
@@ -1,23 +1,24 @@
 package com.cowaine.coalong.chapter12;
 
-import lombok.RequiredArgsConstructor;
-
 // 위치를 나타내는 클래스
-@RequiredArgsConstructor
 public class Location {
     private final int x;
     private final int y;
+
+    Location(final int x, final int y) {
+        if (!valid(x, y)) {
+            throw new IllegalArgumentException("잘못된 위치입니다.");
+        }
+        this.x = x;
+        this.y = y;
+    }
 
     //위치 이동하기
     Location shift(final int shiftX, final int shiftY) {
         int nextX = x + shiftX;
         int nextY = y + shiftY;
-        if (valid(nextX, nextY)) {
-            return new Location(nextX, nextY);
-        }
 
-        // (-1, -1)은 오류 값
-        return new Location(-1, -1);
+        return new Location(nextX, nextY);
     }
 
     private boolean valid(int nextX, int nextY) {
