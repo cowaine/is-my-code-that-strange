0543902f05f43ecd260eff3a2b05dd85c43242f9
cororing <corock.dev@gmail.com>
2024-07-25 17:36:14 +0900

    feat: 값 전달 실수

--- corock/src/main/java/com/cowaine/corock/chapter12/Book.java
+++ corock/src/main/java/com/cowaine/corock/chapter12/Book.java
@@ -1,5 +1,6 @@
 package com.cowaine.corock.chapter12;
 
+import com.cowaine.corock.chapter12.domain.DeliveryCharge;
 import com.cowaine.corock.chapter12.domain.Price;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,6 +16,10 @@ public class Book {
         int deliveryPrice = calcDeliveryPrice(discountedPrice);     // 배송비
 
         log.info("deliveryPrice: {}", deliveryPrice);
+
+        // 배송 수수료 DeliveryCharge 에는 배송비가 전달되어야 하는데 상품 가격 합계가 전달되고 있음.
+        DeliveryCharge deliveryCharge = new DeliveryCharge(price);
+        log.info("deliveryCharge: {}", deliveryCharge);
     }
 
     private static int calcDiscountedPrice(int price) {
---
+++ corock/src/main/java/com/cowaine/corock/chapter12/domain/DeliveryCharge.java
@@ -0,0 +1,12 @@
+package com.cowaine.corock.chapter12.domain;
+
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@ToString
+public class DeliveryCharge {
+
+    private final int amount;
+
+}
