823a13fab7820d9f7906688bdf956cbfcb2202d3
cororing <corock.dev@gmail.com>
2024-07-25 17:24:42 +0900

    feat: 묻지 말고 명령하라

---
+++ corock/src/main/java/com/cowaine/corock/chapter12/domain/Person.java
@@ -0,0 +1,17 @@
+package com.cowaine.corock.chapter12.domain;
+
+public class Person {
+
+    private String name;
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    // setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+}
