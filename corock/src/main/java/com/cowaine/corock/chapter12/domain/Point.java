ceb8b7003450b1b56f5c0fd1c463c49b49b127cd
cororing <corock.dev@gmail.com>
2024-07-25 17:27:37 +0900

    feat: 상태 변경과 추출을 동시에 실행하는 메서드

---
+++ corock/src/main/java/com/cowaine/corock/chapter12/domain/Point.java
@@ -0,0 +1,12 @@
+package com.cowaine.corock.chapter12.domain;
+
+public class Point {
+
+    private int point;
+
+    int gainAndGetPoint() {
+        point += 10;
+        return point;
+    }
+
+}
