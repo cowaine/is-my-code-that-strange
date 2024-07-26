01e5b251eef29756950be1c2e7de078fd6412c5b
cororing <corock.dev@gmail.com>
2024-07-26 17:01:08 +0900

    feat: 악마를 불러들이기 쉬운 사용자 클래스

---
+++ corock/src/main/java/com/cowaine/corock/chapter13/auth/User.java
@@ -0,0 +1,10 @@
+package com.cowaine.corock.chapter13.auth;
+
+class User {
+
+    int id;                     // 식별 ID
+    String name;                // 이름
+    String email;               // 이메일 주소
+    String passwordDigest;      // 비밀번호
+
+}
