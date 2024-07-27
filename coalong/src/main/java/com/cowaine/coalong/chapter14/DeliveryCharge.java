6765ecf1646a525459cf765efe2b680ec249f171
coalong <minay0904@gmail.com>
2024-07-27 16:53:21 +0900

    [chapter14] 배송비 클래스의 최종 형태

--- coalong/src/main/java/com/cowaine/coalong/chapter14/DeliveryCharge.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter14/DeliveryCharge.java
@@ -9,11 +9,10 @@ public class DeliveryCharge {
     private static final int PAY_CHARGE = 5000;
     private static final int CHARGE_FREE = 0;
 
+    /**
+     * @param shoppingCart 장바구니
+     */
     DeliveryCharge(final ShoppingCart shoppingCart) {
-        if (shoppingCart.totalPrice() < CHARGE_FREE_THRESHOLD) {
-            amount = PAY_CHARGE;
-        } else {
-            amount = CHARGE_FREE;
-        }
+        amount = (shoppingCart.totalPrice() < CHARGE_FREE_THRESHOLD) ? PAY_CHARGE : CHARGE_FREE;
     }
 }
