84d65e2ed1b138f8c5edd405770f63d7ec073ad9
coalong <minay0904@gmail.com>
2024-07-27 16:40:46 +0900

    [chapter14] 기본 형태에서 리팩터링 대상 로직 호출하기

--- coalong/src/main/java/com/cowaine/coalong/chapter14/DeliveryCharge.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter14/DeliveryCharge.java
@@ -7,12 +7,6 @@ public class DeliveryCharge {
     private final int amount;
 
     DeliveryCharge(final ShoppingCart shoppingCart) {
-        int totalPrice = shoppingCart.products.get(0).getPrice() + shoppingCart.products.get(1).getPrice();
-
-        if (totalPrice < 20000) {
-            amount = 5000;
-        } else {
-            amount = 0;
-        }
+        amount = DeliveryManager.deliveryCharge(shoppingCart.products);
     }
 }
