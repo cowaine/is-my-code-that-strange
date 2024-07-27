4e5b59f6a3bb99e4807f3547ce26c7b18e4b9a23
cororing <corock.dev@gmail.com>
2024-07-27 14:03:21 +0900

    refactor: 무턱대고 작성한 로직을 목적을 나타내는 메서드로 바꾸기

--- corock/src/main/java/com/cowaine/corock/chapter14/webtoon/Customer.java
+++ corock/src/main/java/com/cowaine/corock/chapter14/webtoon/Customer.java
@@ -18,4 +18,12 @@ public class Customer {
         return !this.isEnabled();
     }
 
+    /**
+     * @param comic 구매 대상 웹툰
+     * @return 보유 포인트가 부족하다면 true
+     */
+    boolean isShortOfPoint(Comic comic) {
+        return possessionPoint.getAmount() < comic.getCurrentPurchasePoint().getAmount();
+    }
+
 }
