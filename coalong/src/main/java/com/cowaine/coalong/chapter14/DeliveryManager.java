4d95f901d0d258138fb240fd52c61e78effc2f10
coalong <minay0904@gmail.com>
2024-07-27 16:49:42 +0900

    [chapter14] 필요없는 메서드 제거

--- coalong/src/main/java/com/cowaine/coalong/chapter14/DeliveryManager.java
+++ coalong/src/main/java/com/cowaine/coalong/chapter14/DeliveryManager.java
@@ -1,25 +1,9 @@
 package com.cowaine.coalong.chapter14;
 
-import java.util.List;
 
 /**
  * 배송 관리 클래스
  */
 public class DeliveryManager {
 
-    /**
-     * 배송비를 리턴함.
-     *
-     * @param shoppingCart 배송 대상 상품 리스트
-     * @return 배송비
-     */
-    public static int deliveryCharge(ShoppingCart shoppingCart) {
-        int charge = 0;
-        if (shoppingCart.totalPrice() < 20000) {
-            charge = 5000;
-        } else {
-            charge = 0;
-        }
-        return charge;
-    }
 }
