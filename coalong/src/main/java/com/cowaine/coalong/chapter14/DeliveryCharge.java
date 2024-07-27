9bee87d03e1f6f5fa35e39c40054fe53a586c07e
coalong <minay0904@gmail.com>
2024-07-27 16:19:50 +0900

    [chapter14] 배송비를 나타내는 클래스의 기본 형태

---
+++ coalong/src/main/java/com/cowaine/coalong/chapter14/DeliveryCharge.java
@@ -0,0 +1,9 @@
+package com.cowaine.coalong.chapter14;
+
+public class DeliveryCharge {
+    private final int amount;
+
+    DeliveryCharge(final ShoppingCart shoppingCart) {
+        amount = -1;
+    }
+}
