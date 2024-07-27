066e0e2d568b34aba3f3cb8c8288f9d2a5dc4a53
cororing <corock.dev@gmail.com>
2024-07-27 13:59:27 +0900

    refactor: 중첩을 제거하여 보기 좋게 만들기

--- corock/src/main/java/com/cowaine/corock/chapter14/webtoon/PurchasePointPayment.java
+++ corock/src/main/java/com/cowaine/corock/chapter14/webtoon/PurchasePointPayment.java
@@ -10,22 +10,21 @@ class PurchasePointPayment {
     final LocalDateTime paymentDatetime;        // 구매 일자
 
     PurchasePointPayment(final Customer customer, final Comic comic) {
-        if (customer.isEnabled()) {
-            this.customerId = customer.getId();
-            if (comic.isEnabled()) {
-                this.comicId = comic.getId();
-                if (comic.getCurrentPurchasePoint().getAmount() <= customer.getPossessionPoint().getAmount()) {
-                    this.consumptionPoint = comic.getCurrentPurchasePoint();
-                    this.paymentDatetime = LocalDateTime.now();
-                } else {
-                    throw new RuntimeException("보유하고 있는 포인트가 부족합니다.");
-                }
-            } else {
-                throw new IllegalArgumentException("현재 구매할 수 없는 만화입니다.");
-            }
-        } else {
+        if (!customer.isEnabled()) {
             throw new IllegalArgumentException("유효하지 않은 계정입니다.");
         }
+        this.customerId = customer.getId();
+
+        if (!comic.isEnabled()) {
+            throw new IllegalArgumentException("현재 구매할 수 없는 만화입니다.");
+        }
+        this.comicId = comic.getId();
+
+        if (customer.getPossessionPoint().getAmount() < comic.getCurrentPurchasePoint().getAmount()) {
+            throw new RuntimeException("보유하고 있는 포인트가 부족합니다.");
+        }
+        this.consumptionPoint = comic.getCurrentPurchasePoint();
+        this.paymentDatetime = LocalDateTime.now();
     }
 
 }
