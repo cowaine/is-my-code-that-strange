7657fb391ec7be1e860d2743b181999e739c8cab
cororing <corock.dev@gmail.com>
2024-07-29 13:02:14 +0900

    feat: 게임 애플리케이션 starter-code 추가 (#124)

---
+++ corock/src/main/java/com/cowaine/corock/mission/game/GameClient.java
@@ -0,0 +1,31 @@
+package com.cowaine.corock.mission.game;
+
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+@RequiredArgsConstructor
+public class GameClient {
+
+    private final String id;
+
+    void connect(String serverHost, int port) {
+        try {
+            Socket socket = new Socket(serverHost, port);
+            System.out.println("Connected to server: " + serverHost + ":" + port);
+
+            Thread sender = new Sender(socket, id);
+            Thread receiver = new Receiver(socket);
+
+            sender.start();
+            receiver.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
